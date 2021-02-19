99b917505ae910461e345f2e4f5d8e2ddc384c57
Itunu Raimi <chavitunu@gmail.com>
2021-02-19 15:44:08 +0100

    Initial Commit

---
+++ Learn By Doing/Learn_By_DoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_By_DoingApp.swift
+//  Learn By Doing
+//
+//  Created by Itunu Raimi on 19/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct Learn_By_DoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
